933c8ae0647be0a895dd282960eeb69e9ba8e8d2
Petya Angelova <bedfordpetya@gmail.com>
2022-09-06 21:11:58 +0300

    added about me popup

--- src/main.js
+++ src/main.js
@@ -7,3 +7,14 @@ function toggleButton(){
 
 hamburgerButton.addEventListener('click', toggleButton)
 
+//open modal
+const aboutMeButton = document.getElementById('about-me-button')
+
+aboutMeButton.addEventListener('click', () =>{
+    document.querySelector('.bg-modal').style.display = 'flex'
+})
+
+document.querySelector('.close').addEventListener('click', () =>{
+    document.querySelector('.bg-modal').style.display = 'none'
+})
+
